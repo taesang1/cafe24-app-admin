3a5a06b349d2975771b7ab31c625d7352325bde7
taesang <tsjung@intellisys.co.kr>
2022-07-28 10:36:43 +0900

    js 코드 난독화

--- main/uglify.sh
+++ main/uglify.sh
@@ -1,2 +1,2 @@
-uglifyjs js/cart.js -o js/uglify/test.min.js -c -m
-uglifyjs js/cart_log.js -o js/uglify/test_2.min.js -c -m
+uglifyjs js/prdDetail_code.js -o js/uglify/prdDetail_code.min.js -c -m
+uglifyjs js/prdDetail_similar.js -o js/uglify/prdDetail_similar.min.js -c -m
